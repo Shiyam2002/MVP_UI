f838f965acbf96e394742a70d7550777dc18bc7e
Shiyam.K.S <k.s.shiyam2002@gmail.com>
2025-12-14 23:14:40 +0530

    feat: added sidebar

--- axora/src/app/page.tsx
+++ axora/src/app/page.tsx
@@ -1,8 +1,12 @@
 import Login04 from "../components/login";
+import Sidebar03 from "../components/sidebar-03";
 
 
 export default function Home() {
   return (
+    <>
     <Login04 />
+      
+    </>
   );
 }
